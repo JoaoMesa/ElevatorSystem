e0dc5603742d20fde32db9f7a624447e0a74b3db
JoaoMesa <navesmesajoao@gmail.com>
2023-10-12 17:36:38 -0300

    Criando gerenciador

--- src/Gerenciador.py
+++ src/Gerenciador.py
@@ -1,5 +1,32 @@
-class Gerenciador:
-  filaDeChamadas = []
-  def __init__(self):
-    pass
+from Elevador import Elevador
+from Chamada import Chamada
 
+class Gerenciador:
+
+  def __init__(self):
+      self.e1 = Elevador()
+      self.filaDeChamadas = []
+      self.run()
+
+
+  def run(self):
+    while True:
+      self.e1.info()
+      escolha = int(input("O que deseja fazer?\n 0 - Sair \n 1 - Chamar elevador \n 2 - Esperar \n"))
+
+      if escolha == 0:
+        break
+  
+      if escolha == 1:
+        ch = Chamada(int(input("Qual andar você está?\n")))
+        if ch.andar == self.e1.andar:
+          ch = Chamada(int(input("Pra onde você quer ir?\n")))
+        self.filaDeChamadas.append(ch)
+
+      if self.e1.alvo == self.e1.andar and len(self.filaDeChamadas) > 0:
+        temp = self.filaDeChamadas[0].andar
+        self.e1.mudaAlvo(temp)
+
+      self.e1.mudaAndar()
+      if len(self.filaDeChamadas) > 0 and self.e1.andar == self.filaDeChamadas[0].andar:
+        self.filaDeChamadas.pop(0)
--- src/main.py
+++ src/main.py
@@ -1,24 +1,2 @@
-e1 = Elevador()
-filaDeChamadas = []
-
-
-while True:
-  e1.info()
-  escolha = int(input("O que deseja fazer?\n 0 - Sair \n 1 - Chamar elevador \n 2 - Esperar \n"))
-
-  if escolha == 0:
-    break
-
-  if escolha == 1:
-    ch = Chamada(int(input("Qual andar você está?\n")))
-    if ch.andar == e1.andar:
-      ch = Chamada(int(input("Pra onde você quer ir?\n")))
-    filaDeChamadas.append(ch)
-
-  if e1.alvo == e1.andar and len(filaDeChamadas) > 0:
-    temp = filaDeChamadas[0].andar
-    e1.mudaAlvo(temp)
-
-  e1.mudaAndar()
-  if len(filaDeChamadas)> 0 and e1.andar == filaDeChamadas[0].andar:
-    filaDeChamadas.pop(0)
+from Gerenciador import Gerenciador
+g1 = Gerenciador()
